af81c9855bb3cbbc9f59c7c70b1aa5beba836ec4
Max Irwin <max_irwin@yahoo.com>
2024-01-26 16:33:36 -0500

    Labs updated

--- ai-search-lab/sync-container-to-local.sh
+++ ai-search-lab/sync-container-to-local.sh
@@ -9,6 +9,7 @@ docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/03-index-opensearch.ipynb" "$DESTINAT
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/04-query-hybrid.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/05-RAG.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/06-Text2Query.ipynb" "$DESTINATION_DIR"
+docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/07-CLIP.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/requirements.txt" "$DESTINATION_DIR"
 
 #cc_news_title_embeddings_50000.pkl
