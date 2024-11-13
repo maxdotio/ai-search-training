dd891ea7f819dc960c79de18b5b7b7e3aef35f41
Max Irwin <max_irwin@yahoo.com>
2024-11-13 10:09:51 -0500

    Structured Output notebook

--- ai-search-lab/sync-container-to-local.sh
+++ ai-search-lab/sync-container-to-local.sh
@@ -11,6 +11,7 @@ docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/05-RAG.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/06-Text2Query.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/07-CLIP.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/08-chain-of-thought.ipynb" "$DESTINATION_DIR"
+docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/09-structured-output.ipynb" "$DESTINATION_DIR"
 docker cp "$SOURCE_CONTAINER":"$SOURCE_DIR/requirements.txt" "$DESTINATION_DIR"
 
 #cc_news_title_embeddings_50000.pkl
